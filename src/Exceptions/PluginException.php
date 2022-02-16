5ff263f603c85aba02aef27ae3a330f923956e0e
啊正 <770412272@qq.com>
2022-02-16 10:20:39 +0800

    调整缓存的key

--- src/Exceptions/PluginException.php
+++ src/Exceptions/PluginException.php
@@ -11,12 +11,12 @@ class PluginException extends \RuntimeException
 {
     public function __construct($code = 0, $message = '', Throwable $previous = null)
     {
-        if (!$message && is_string($message)) {
+        if (!$message) {
             $message = ErrorCode::getMessage($code);
-        }
-
-        if (!$message && is_array($message)) {
-            $message = ErrorCode::getMessage($code, $message);
+        }else{
+            if (is_array($message)) {
+                $message = ErrorCode::getMessage($code, $message);
+            }
         }
 
         parent::__construct($message, $code, $previous);
